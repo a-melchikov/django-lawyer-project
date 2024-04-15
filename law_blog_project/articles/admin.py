d91e10c63a3c43719d94af744faa0d1122bbde00
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-04-15 23:07:32 +0400

    A convenient admin panel has been created

--- law_blog_project/articles/admin.py
+++ law_blog_project/articles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at")
+    list_filter = ("author", "created_at")
+    search_fields = ("title", "content")
+    ordering = ("-created_at",)
--- law_blog_project/articles/models.py
+++ law_blog_project/articles/models.py
@@ -4,10 +4,16 @@ from django.utils import timezone
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    content = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    author = models.CharField(max_length=100, verbose_name="Автор")
+    content = models.TextField(verbose_name="Текст")
+    created_at = models.DateTimeField(
+        default=timezone.now, verbose_name="Дата публикации"
+    )
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
 
     def get_absolute_url(self):
         return reverse("article_detail", args=[str(self.id)])
